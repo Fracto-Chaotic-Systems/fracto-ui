aa6a89d45988be8cc7469ef97493dad0e63db8e0
media-frameworks <mikehallvideo@gmail.com>
2026-04-05 15:18:45 -0400

    change hypertest

--- src/pages/study/StudyOverview.jsx
+++ src/pages/study/StudyOverview.jsx
@@ -15,7 +15,7 @@ export class StudyOverview extends Component {
          <styles.CenteredBlock
             key={'input-form'}>
             <FractoRasterImage
-               width_px={500}
+               width_px={150}
                focal_point={{x: 0, y: 0}}
                scope={10}
                data_endpoint={'hyper_canvas_buffer'}
